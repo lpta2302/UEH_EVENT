251bcff670c0be1570abbbe0a8bc60ea19c34f7a
Tien Phi <phivu7747@gmail.com>
2024-03-29 08:58:21 +0700

    add game

--- UEH_EVENT/DA/models/Game.cs
+++ UEH_EVENT/DA/models/Game.cs
@@ -8,6 +8,7 @@ public class Game
     public int Id { get; set; }
     [StringLength(20)]
     public string Name { get; set; }
+    public string Data { get; set; }
     public Game(string name = "")
     {
         Name = name;
